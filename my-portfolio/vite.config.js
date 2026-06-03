37b611197c20860f466f0717861fd1824a51bbe4
thebluehomosapien <70254940+thebluehomosapien@users.noreply.github.com>
2026-06-03 16:23:07 -0700

    made edit

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  resolve: {
+    dedupe: ['react', 'react-dom', 'three'],
+  },
 })
